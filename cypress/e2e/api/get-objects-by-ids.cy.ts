04a7c7bd79770a3ec10d514b8bf06fc167d658a6
Elcio Filho <elcio.filho@hypera.com.br>
2026-09-14 14:56:18 -0300

    test: cover device lookup API scenarios

---
+++ cypress/e2e/api/get-objects-by-ids.cy.ts
@@ -0,0 +1,40 @@
+interface Device {
+  id: string
+  name: string
+}
+
+describe('GET /objects by multiple IDs', () => {
+  it('should return the requested objects successfully', () => {
+    const requestedIds = ['3', '5', '10']
+
+    cy.log('Request objects by multiple IDs')
+
+    cy.request({
+      method: 'GET',
+      url: 'https://api.restful-api.dev/objects',
+      qs: {
+        id: requestedIds,
+      },
+    }).then((response) => {
+      cy.log('Validate successful response')
+
+      expect(response.status).to.eq(200)
+      expect(response.body).to.be.an('array')
+      expect(response.body).to.have.length(3)
+
+      cy.log('Validate returned object IDs')
+
+      const devices = response.body as Device[]
+      const returnedIds = devices.map((device) => device.id)
+
+      expect(returnedIds).to.have.members(requestedIds)
+
+      cy.log('Validate object structure')
+
+      devices.forEach((device) => {
+        expect(device).to.have.property('id')
+        expect(device).to.have.property('name')
+      })
+    })
+  })
+})
