c195fd047c3bdabd9a81d9fcaad0be28178256f0
Elcio Filho <elcioasf@gmail.com>
2026-09-14 00:37:53 -0300

    test: cover get all devices endpoint

---
+++ cypress/e2e/api/get-all-objects.cy.ts
@@ -0,0 +1,21 @@
+describe('GET /objects', () => {
+  it('should return the list of objects successfully', () => {
+    cy.log('Request the list of objects')
+
+    cy.request('https://api.restful-api.dev/objects')
+      .then((response) => {
+        cy.log('Validate successful response')
+
+        expect(response.status).to.eq(200)
+        expect(response.body).to.be.an('array')
+        expect(response.body.length).to.be.greaterThan(0)
+
+        cy.log('Validate object structure')
+
+        response.body.forEach((object) => {
+          expect(object).to.have.property('id')
+          expect(object).to.have.property('name')
+        })
+      })
+  })
+})
